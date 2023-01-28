ad381a90582d01b48104b64e9fddb89ec6da556d
onthelock <s.chalormsak@gmail.com>
2023-01-28 12:33:10 +0700

    comment main

--- C-01/ex01/ft_ultimate_ft.c
+++ C-01/ex01/ft_ultimate_ft.c
@@ -10,7 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
+/*
+int	main(void)
+{
+	int	n;
+	int	*n1;
+	int	**n2;
+	int	***n3;
+	int	****n4;
+	int	*****n5;
+	int	******n6;
+	int	*******n7;
+	int	********n8;
+	int	*********n9;
+
+	n1 = &n;
+	n2 = &n1;
+	n3 = &n2;
+	n4 = &n3;
+	n5 = &n4;
+	n6 = &n5;
+	n7 = &n6;
+	n8 = &n7;
+	n9 = &n8;
+	ft_ultimate_ft(n9);
+	printf("%d\n", n);
+	return (0);
+}
+*/
