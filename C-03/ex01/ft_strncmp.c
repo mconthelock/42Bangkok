1b275dbca2ee81b358090c15845e8ac64d33358d
onthelock <s.chalormsak@gmail.com>
2023-01-28 20:28:13 +0700

    last update

--- C-03/ex01/ft_strncmp.c
+++ C-03/ex01/ft_strncmp.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: csewanam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/26 21:21:57 by csewanam          #+#    #+#             */
-/*   Updated: 2023/01/26 23:20:59 by csewanam         ###   ########.fr       */
+/*   Created: 2023/01/28 16:07:19 by csewanam          #+#    #+#             */
+/*   Updated: 2023/01/28 16:07:29 by csewanam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
+    unsigned int	i;
 
 	i = 0;
 	while ((s1[i] == s2[i]) && (s1[i] != '\0' && s2[i] != '\0'))
@@ -24,20 +24,8 @@ int	ft_strcmp(char *s1, char *s2)
 	}
 	return (s1[i] - s2[i]);
 }
-/*
-int	main(void)
+
+int main(void)
 {
-	char	*s1;
-	char	*s2;
-	int		cmp1;
-	int		cmp2;
-	
-	s1 = "1234";
-	s2 = "4455";
-	cmp1 = strcmp(s1, s2);
-	printf("Compare By STMCMP : %d\n", cmp1);
-	cmp2 = ft_strcmp(s1, s2);
-	printf("Compare By FT_STRCMP : %d\n", cmp2);
-	return (0);
+    return (0);
 }
-*/
