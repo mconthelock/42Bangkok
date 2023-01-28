fbcdfb3e20e104324fbe173ab74ef690b3c43ebf
onthelock <s.chalormsak@gmail.com>
2023-01-28 09:22:56 +0700

    comment main

--- C-02/ex03/ft_str_is_numeric.c
+++ C-02/ex03/ft_str_is_numeric.c
@@ -6,9 +6,11 @@
 /*   By: csewanam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 23:27:19 by csewanam          #+#    #+#             */
-/*   Updated: 2023/01/23 23:42:07 by csewanam         ###   ########.fr       */
+/*   Updated: 2023/01/28 09:21:09 by csewanam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_str_is_numeric(char *str)
 {
@@ -25,3 +27,14 @@ int	ft_str_is_numeric(char *str)
 	}
 	return (1);
 }
+/*
+int	main(void)
+{
+	char	*c;
+	int		n;
+
+	c = "1234";
+	n = ft_str_is_numeric(c);
+	printf("%d\n", n);
+	return (0);
+}*/
