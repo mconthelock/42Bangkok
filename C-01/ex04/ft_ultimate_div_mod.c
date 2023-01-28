11519c78676e8126d44f49a21fdd4afb010b7f86
onthelock <s.chalormsak@gmail.com>
2023-01-28 12:45:00 +0700

    comment main

--- C-01/ex04/ft_ultimate_div_mod.c
+++ C-01/ex04/ft_ultimate_div_mod.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 void	ft_ultimate_div_mod(int *a, int *b)
 {
 	int	i;
@@ -23,3 +25,21 @@ void	ft_ultimate_div_mod(int *a, int *b)
 		*b = i % j;
 	}
 }
+/*
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	*div;
+	int	*mod;
+
+	a = 12;
+	b = 4;
+	div = &a;
+	mod = &b;
+	printf("Before: Div => %d, Mod => %d\n", a, b);
+	ft_ultimate_div_mod(div, mod);
+	printf("After: Div => %d, Mod => %d\n", a, b);
+	return (0);
+}
+*/
