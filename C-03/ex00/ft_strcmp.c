86937685cf1c65ec4a537685f1ceb86086d42bd3
onthelock <s.chalormsak@gmail.com>
2023-01-28 16:03:42 +0700

    ex00 => OK

--- C-03/ex00/ft_strcmp.c
+++ C-03/ex00/ft_strcmp.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
+
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
@@ -23,3 +25,20 @@ int	ft_strcmp(char *s1, char *s2)
 	}
 	return (s1[i] - s2[i]);
 }
+/*
+int	main(void)
+{
+	char	*s1;
+	char	*s2;
+	int		cmp1;
+	int		cmp2;
+	
+	s1 = "1234";
+	s2 = "4455";
+	cmp1 = strcmp(s1, s2);
+	printf("Compare By STMCMP : %d\n", cmp1);
+	cmp2 = ft_strcmp(s1, s2);
+	printf("Compare By FT_STRCMP : %d\n", cmp2);
+	return (0);
+}
+*/
