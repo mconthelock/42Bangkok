67d591e8ae18f342ef2995817f87b46f97246ac4
onthelock <s.chalormsak@gmail.com>
2023-01-30 09:14:56 +0700

    update norm

--- C-03/ex01/ft_strncmp.c
+++ C-03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: csewanam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 16:07:19 by csewanam          #+#    #+#             */
-/*   Updated: 2023/01/30 09:10:13 by csewanam         ###   ########.fr       */
+/*   Updated: 2023/01/30 09:14:36 by csewanam         ###   ########.fr       */
 /*   Updated: 2023/01/28 20:50:00 by csewanam         ###   ########.fr       */
 /*   Updated: 2023/01/29 00:37:57 by csewanam         ###   ########.fr       */
 /*                                                                            */
@@ -33,14 +33,14 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	}
 	return (s1[i - 1] - s2[i - 1]);
 }
-
+/*
 int main(void)
 {
     char    s1[] = "1734";
-    char    s2[] = "\0A35";
+    char    s2[] = "17A35";
 
     printf("%d\n", ft_strncmp(s1, s2, 3));
     printf("%d\n", strncmp(s1, s2, 3));
     return (0);
 }
-
+*/
