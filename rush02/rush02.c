40da378d0dbae9ac20d3d91394f4e6ba7c52679c
Chalormsak Sewanam <s.chalormsak@gmail.com>
2023-01-30 20:18:52 +0700

    update rush

--- rush02/rush02.c
+++ rush02/rush02.c
@@ -6,7 +6,7 @@
 /*   By: csewanam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 13:03:58 by csewanam          #+#    #+#             */
-/*   Updated: 2023/01/29 13:13:52 by csewanam         ###   ########.fr       */
+/*   Updated: 2023/01/29 17:04:43 by csewanam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,87 +15,92 @@
 # include <fcntl.h>
 # include <stdio.h>
 
-char	*ft_strstr(char *str, char *to_find);
+struct letter {
+	char	*value;
+	char	*label;
+};
 
-char	*ft_readfile(char *dict, char *filename)
-{
-	char	*allocate; 
-	int	files;
-	int	buffer;
-	int	i;
+struct letter letters[20];
 
-	allocate = (char*)malloc(2000 * sizeof(char));
-	files = open(filename, O_RDONLY);
-	if (files < 0)
-	{
-		write(1, "Error\n", 20);
-		return (0);
-	}
-	buffer = read(files, allocate, 2000);
-	if (buffer < 0)
-	{
-		write(1, "Error\n", 20);
-		return (0);
-	}
-	dict = (char*)malloc((buffer + 1) * sizeof(char));
-	i = 0;
-	while (i < buffer)
-	{
-		dict[i] = allocate[i];
-		i++;
-	}
-	free(allocate);
-	return (dict);
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-int	ft_power(int d)
+void	ft_set_letters(char *str)
 {
 	int i;
-	int n;
-
+	int	j;
+	int	k;
+	char	*n;
+	
 	i = 0;
-	n = 1;
-	while (i < d)
-	{
-		n = n*10;
-		i++;
-	}
-	return n;
-}
-
-
-void	ft_print_dict(char *str, int len, char *dict)
-{
-	int i = 0;
+	j = 0;
+	k = 0;
+	n = (char*)malloc(20 * sizeof(char));
 	while (str[i] != '\0')
 	{
-		ft_strstr(*dict, ft_power(i));
-		//printf("%d\n", ft_power(i));
+		if (str[i] == ':')
+		{
+			letters[j].value = n;
+			free(n);
+			n = (char*)malloc(20 * sizeof(char));
+			k = 0;
+		}
+		else if(str[i] == '\n')
+		{
+			letters[j].label = n;
+			free(n);
+			n = (char*)malloc(20 * sizeof(char));
+			printf("%s => %s\n", letters[j].value, letters[j].label);
+			k = 0;
+			j++;	
+		}
+		else
+		{
+			n[k] = str[i];	
+			k++;
+		}
 		i++;
 	}
-
+	free(n);
 }
 
-int	main(int argc, char *argv[])
+char    *ft_readfile(char *dict, char *filename)
 {
-	char	*filename = "numbers.dict";
-	char	*dict;
-	int	len;
+    char    *allocate; 
+    int    files;
+    int    buffer;
+    int    i;
 
-	dict = ft_readfile(dict, filename);
-	len  = ft_strlen(argv[1]);
-	ft_print_dict(argv[1], len, dict);
-	return (0);
+    allocate = (char*)malloc(2000 * sizeof(char));
+    files = open(filename, O_RDONLY);
+    if (files < 0)
+    {
+        write(1, "Error\n", 20);
+        return (0);
+    }
+    buffer = read(files, allocate, 2000);
+    dict = (char*)malloc((buffer + 1) * sizeof(char));
+    i = 0;
+    while (i < buffer)
+    {
+        dict[i] = allocate[i];
+        i++;
+    }
+	free(allocate);
+    return (dict);
+}
+
+int    main(int argc, char *argv[])
+{
+    char    *filename = "numbers.dict";
+    char    *dict;
+	int	i;
+
+    dict = ft_readfile(dict, filename);
+	ft_set_letters(dict);
+	
+	i = 0;
+	//while (letters[i] != '\0')
+	//{
+		//printf("%s\n", letters[1].label);
+	//}
+	free(dict);
+    return (0);
 }
