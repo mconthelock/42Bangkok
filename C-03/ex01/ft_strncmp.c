6875cdf0fa7828386b632a13b110adddbe1f88cf
onthelock <s.chalormsak@gmail.com>
2023-01-30 08:49:12 +0700

    update && to ||

--- C-03/ex01/ft_strncmp.c
+++ C-03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: csewanam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 16:07:19 by csewanam          #+#    #+#             */
-/*   Updated: 2023/01/29 00:37:57 by csewanam         ###   ########.fr       */
+/*   Updated: 2023/01/30 08:47:24 by csewanam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while ((i < n) && (s1[i] != '\0' && s2[i] != '\0'))
+	while ((i < n) || (s1[i] != '\0' && s2[i] != '\0'))
 	{
 		if (s1[i] == s2[i])
 		{
@@ -31,14 +31,14 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	}
 	return (s1[i -1] - s2[i - 1]);
 }
-/*
+
 int main(void)
 {
     char    s1[] = "1734";
-    char    s2[] = "1A35";
+    char    s2[] = "\0A35";
 
     printf("%d\n", ft_strncmp(s1, s2, 3));
     printf("%d\n", strncmp(s1, s2, 3));
     return (0);
 }
-*/
+
